06a3097a7226658772825cc4aa20a22c5ee3f195
Mizofabio <yokomizo.fabio@gmail.com>
2023-08-23 16:39:43 -0300

    Foi feito alteração da strings.xml e criado o name bem vindo para modificar o nome do app

--- app/src/main/java/com/teste/meuprimeiroappdio/MainActivity.kt
+++ app/src/main/java/com/teste/meuprimeiroappdio/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("World!")
                 }
             }
         }
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name",
         modifier = modifier
     )
 }
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MeuPrimeiroAppDioTheme {
-        Greeting("Android")
+        Greeting("World!")
     }
 }
--- app/src/main/java/com/teste/meuprimeiroappdio/ui/theme/Theme.kt
+++ app/src/main/java/com/teste/meuprimeiroappdio/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.teste.meuprimeiroappdio.ui.theme
+ package com.teste.meuprimeiroappdio.ui.theme
 
 import android.app.Activity
 import android.os.Build
